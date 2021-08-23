60051807ded08960bb6ae152d798dd858db5cbf5
Dylan <74956947+DylanThomasShine@users.noreply.github.com>
2021-08-23 17:04:12 +0100

    Update env.py

--- env.py
+++ env.py
@@ -1,1 +1,7 @@
+import os
 
+os.environ.setdefault("IP", "0.0.0.0")
+os.environ.setdefault("PORT", "5000")
+os.environ.setdefault("SECRET_KEY", "%B-oy^,&kN'f^SJkGXu=n3,m/fnUC0")
+os.environ.setdefault("MONGO_URI", "mongodb+srv://root:Krypton128@myfirtcluster.wekhl.mongodb.net/task_manager?retryWrites=true&w=majority")
+os.environ.setdefault("MONGO_DBNAME", "task_manager”)
